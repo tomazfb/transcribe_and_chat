116a477ecbebe8b69bad17131582efdfcc7c3a37
tomazfb <tomazfb@hotmail.com>
2023-10-18 13:09:31 -0300

    inicio interface web

--- app.py
+++ app.py
@@ -1,70 +1,52 @@
+import streamlit as st
 from transcricao import Transcricao
 from chat_with_embeddings import ChatWithEmbeddings
 import os
-import sys
-from langchain.callbacks import get_openai_callback
-
-def main(args):
-    # o argumento deve ser um arquivo.mp3 ou arquivo.wav
-    if len(args) != 2:
-        # imprime a mensagem de uso permitindo mp3 ou wav
-        print("Uso: python app.py <arquivo_entrada>")
-        return
-
-    nome_arquivo = args[1]
-
-    if not os.path.isfile(nome_arquivo):
-        print(f"O arquivo '{nome_arquivo}' não foi encontrado.")
-        return
-
-    extension_lowercase = nome_arquivo.split(".")[-1].lower()
-
-    if extension_lowercase.endswith("mp3") or extension_lowercase.endswith("wav"):
-        t = Transcricao(nome_arquivo)
-        transcricao = t.obter_transcricao_audio()
-        # escreve o resultado no caminho
-        # do arquivo com o nome do arquivo sem extensão
-        # e com extensão .txt
-        # exemplo: arquivo.mp3 -> arquivo_transcricao.txt
-        # exemplo: arquivo.wav -> arquivo_transcricao.txt
-        nome_arquivo_split = nome_arquivo.split(".")
-        nome_arquivo_split[-2] += "_transcricao"
-        nome_arquivo_split[-1] = "txt"
-        arquivo_transcricao = ".".join(nome_arquivo_split)
-        with open(arquivo_transcricao, "w") as f:
-            f.write(transcricao)
-
-        print(f"A transcrição foi salva em: {arquivo_transcricao}")
-    elif extension_lowercase.endswith("txt") or extension_lowercase.endswith("xlsx") or extension_lowercase.endswith("csv"):
-        with get_openai_callback() as cb:
-            if extension_lowercase.endswith("txt"):
-                loader = ChatWithEmbeddings.create_text_loader(nome_arquivo)
-            elif extension_lowercase.endswith("csv"):
-                loader = ChatWithEmbeddings.create_csv_loader(nome_arquivo)
-            else: # extension_lowercase.endswith("xlsx"):
-                loader = ChatWithEmbeddings.create_unstructured_excel_loader(nome_arquivo)
-            
-            c = ChatWithEmbeddings(loader)
-            
-            #pergunta ao usuário a frase
-            frase = input(f"Digite o prompt para interagir com {nome_arquivo}: \n")
-            print("")
-            while frase != "q":
-                resposta = c.chat(frase)
-                if resposta and resposta["result"]:
-                    print(resposta["result"])
-                    print("--- Total Token and Cost Tracking ---")
-                    print(cb)
-                    print("---")
-                frase = input(f"Digite o prompt ou 'q' para sair: \n")
-
-    else:
-        raise Exception("O arquivo deve ser um arquivo .mp3, .wav, .txt ou .xlsx")
-
-if __name__ == "__main__":
-    main(sys.argv)
+import pathlib
+import uuid
 
 
+def show():
+    st.title('Transcribe And Chat')
 
+    st.write('')
 
+    uploaded_file = st.file_uploader('Upload a file',
+                            help="Upload a file in .wav or .mp3 or a text file in .csv or .txt or even a .xlsx file",
+                            type=['wav', 'mp3', 'txt', 'csv', 'xlsx'],
+                            accept_multiple_files=False)
 
+    if uploaded_file:
+
+        data = uploaded_file.getvalue()
+        
+        # create an unique temporary file name based on a UUID
+        destination_file_name = uuid.uuid4().hex + uploaded_file.name
+
+        # create ./tmp/ directory if it doesn't exist
+        pathlib.Path("./tmp/").mkdir(parents=True, exist_ok=True)
+
+        destination_path = os.path.join("./tmp/", destination_file_name)
+
+        with open(destination_path, "wb") as destination_file:
+            destination_file.write(data)
+            destination_file.close()
+
+            # save the file with a new temporary unique name
+            extension_lowercase = uploaded_file.name.split(".")[-1].lower()
+
+            if extension_lowercase == 'wav' or extension_lowercase == "mp3":
+                if st.button("Transcribe it"):
+                    t = Transcricao(destination_path)
+                    with st.spinner('Transcribing...'):
+                        transcription = t.obter_transcricao_audio()
+                    st.code(transcription)
+                    pass
+            elif extension_lowercase == "txt" or extension_lowercase == "csv" or extension_lowercase == "xlsx":
+                if st.button("Chat it"):
+                    pass
+
+        # remove the temporary file
+        os.remove(destination_path)
+
+show()
---
+++ app_cli.py
@@ -0,0 +1,70 @@
+from transcricao import Transcricao
+from chat_with_embeddings import ChatWithEmbeddings
+import os
+import sys
+from langchain.callbacks import get_openai_callback
+
+def main(args):
+    # o argumento deve ser um arquivo.mp3 ou arquivo.wav
+    if len(args) != 2:
+        # imprime a mensagem de uso permitindo mp3 ou wav
+        print("Uso: python app.py <arquivo_entrada>")
+        return
+
+    nome_arquivo = args[1]
+
+    if not os.path.isfile(nome_arquivo):
+        print(f"O arquivo '{nome_arquivo}' não foi encontrado.")
+        return
+
+    extension_lowercase = nome_arquivo.split(".")[-1].lower()
+
+    if extension_lowercase.endswith("mp3") or extension_lowercase.endswith("wav"):
+        t = Transcricao(nome_arquivo)
+        transcricao = t.obter_transcricao_audio()
+        # escreve o resultado no caminho
+        # do arquivo com o nome do arquivo sem extensão
+        # e com extensão .txt
+        # exemplo: arquivo.mp3 -> arquivo_transcricao.txt
+        # exemplo: arquivo.wav -> arquivo_transcricao.txt
+        nome_arquivo_split = nome_arquivo.split(".")
+        nome_arquivo_split[-2] += "_transcricao"
+        nome_arquivo_split[-1] = "txt"
+        arquivo_transcricao = ".".join(nome_arquivo_split)
+        with open(arquivo_transcricao, "w") as f:
+            f.write(transcricao)
+
+        print(f"A transcrição foi salva em: {arquivo_transcricao}")
+    elif extension_lowercase.endswith("txt") or extension_lowercase.endswith("xlsx") or extension_lowercase.endswith("csv"):
+        with get_openai_callback() as cb:
+            if extension_lowercase.endswith("txt"):
+                loader = ChatWithEmbeddings.create_text_loader(nome_arquivo)
+            elif extension_lowercase.endswith("csv"):
+                loader = ChatWithEmbeddings.create_csv_loader(nome_arquivo)
+            else: # extension_lowercase.endswith("xlsx"):
+                loader = ChatWithEmbeddings.create_unstructured_excel_loader(nome_arquivo)
+            
+            c = ChatWithEmbeddings(loader)
+            
+            #pergunta ao usuário a frase
+            frase = input(f"Digite o prompt para interagir com {nome_arquivo}: \n")
+            print("")
+            while frase != "q":
+                resposta = c.chat(frase)
+                if resposta and resposta["result"]:
+                    print(resposta["result"])
+                    print("--- Total Token and Cost Tracking ---")
+                    print(cb)
+                    print("---")
+                frase = input(f"Digite o prompt ou 'q' para sair: \n")
+
+    else:
+        raise Exception("O arquivo deve ser um arquivo .mp3, .wav, .txt ou .xlsx")
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+
+
+
